d2733463d4a2b324f0fa49b18a70f011111895db
Caleb Mundati <calebngethe230@gmail.com>
2025-07-22 10:30:13 +0300

    adds index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,26 @@
+// Import core React libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// Import global CSS styles
+import './index.css';
+
+// Import the main App component
+import App from './App';
+
+// Import Auth context provider for global state management
+import { AuthProvider } from './context/AuthContext';
+
+// Get the root DOM node where the React app will mount
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the React component tree
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Global Auth Context for managing user state and tokens */}
+    <AuthProvider>
+      {/* Main App component where routing and UI live */}
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
